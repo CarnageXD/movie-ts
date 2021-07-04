e558da8967ec0e3fd0e23a6395e820e0730b8ca3
CarnageXD <bodya21111@gmail.com>
2021-07-04 08:51:50 +0300

    connected to MovieDB to get popular films, defined ts-types for request

--- src/components/Film/Film.tsx
+++ src/components/Film/Film.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
-export const Film = () => {
+type FilmPropsType = {
+    poster?: string | null
+    title?: string
+
+}
+
+export const Film: React.FC<FilmPropsType> = ({ poster, title }) => {
     return (
         <div className="max-w-xs py-10 px-4">
             <div>
-                <img width={300} height={400} src="https://image.tmdb.org/t/p/w500//yvmKPlTIi0xdcFQIFcQKQJcI63W.jpg" alt="poster" />
+                <img width={300} height={400} src={`https://image.tmdb.org/t/p/w500${poster}`} alt="poster" />
             </div>
             <div className="flex items-center flex-col">
-                <h2 className="text-xl text-white p-2 flex-wrap text-center">Shawshank Redemption</h2>
+                <h2 className="text-xl text-white p-2 flex-wrap text-center">{title}</h2>
                 <p className="hover:bg-white hover:text-black transition-all cursor-pointer p-2 text-gray text-lg border-2 w-52 text-center">More details</p>
             </div>
         </div>
--- src/pages/trends/Trends.tsx
+++ src/pages/trends/Trends.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 import { Film } from '../../components/Film/Film'
+import { useEffect } from 'react'
 
-export const Trends = () => {
+type trendMoviesType = {
+    poster_path?: string | null
+    adult?: boolean
+    overview?: string
+    release_date?: string
+    genre_ids?: number[]
+    id?: number
+    original_title?: string
+    original_language?: string
+    title?: string
+    backdrop_path?: string | null
+    popularity?: number
+    vote_count?: number
+    video?: boolean
+    vote_average?: number
+}
+
+export const Trends: React.FC<trendMoviesType> = () => {
+    const [movies, setMovies] = useState<trendMoviesType[]>([])
+    useEffect(() => {
+        axios.get("https://api.themoviedb.org/3/movie/popular?api_key=e5bf0d2a91e3d8acf07245cbd2950b9f&language=en-US&page=2")
+            .then((resp) => setMovies(resp.data.results))
+
+    }, [])
+
     return (
         <div>
             <div className="w-full rounded-lg bg-gray-light flex items-center">
@@ -10,14 +37,9 @@ export const Trends = () => {
             </div>
             <div>
                 <div className="flex justify-between flex-wrap">
-                    <Film />
-                    <Film />
-                    <Film />
-                    <Film />
-                    <Film />
-                    <Film />
-                    <Film />
-                    <Film />
+                    {
+                        movies.map(movie => <Film poster={movie.poster_path} title={movie.original_title} />)
+                    }
                 </div>
             </div>
             <div>hot</div>
